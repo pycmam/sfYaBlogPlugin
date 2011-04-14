6ca81d2e389cc95fc48112ee710aaae03d907c7c
pycmam <pycmam@gmail.com>
2011-04-14 14:14:02 +0600

    Используем TinyMCE в форме создания поста

--- lib/form/doctrine/PluginBlogPostForm.class.php
+++ lib/form/doctrine/PluginBlogPostForm.class.php
@@ -7,12 +7,14 @@ abstract class PluginBlogPostForm extends BaseBlogPostForm
 {
     public function setup()
     {
+        $tinyMCEConfig = sfConfig::get('app_sf_ya_blog_plugin_tiny_mce_config');
+
         $this->setWidgets(array(
             'id' => new sfWidgetFormInputHidden(),
             'title' => new sfWidgetFormInput(),
             'is_published' => new sfWidgetFormInputCheckbox(),
-            'short' => new sfWidgetFormTextarea(),
-            'content' => new sfWidgetFormTextarea(),
+            'short' => new sfWidgetFormTextareaTinyMCE($tinyMCEConfig),
+            'content' => new sfWidgetFormTextareaTinyMCE($tinyMCEConfig),
             'slug' => new sfWidgetFormInput(),
             'meta_title' => new sfWidgetFormInput(),
             'meta_keywords' => new sfWidgetFormTextarea(),
@@ -26,9 +28,9 @@ abstract class PluginBlogPostForm extends BaseBlogPostForm
             'short' => new sfMainPlugin\Validator\sfValidatorString(),
             'content' => new sfMainPlugin\Validator\sfValidatorString(array('required' => false)),
             'slug' => new sfMainPlugin\Validator\sfValidatorSlug(array('max_length' => 50)),
-            'meta_title' => new sfMainPlugin\Validator\sfValidatorString(array('max_length' => 255)),
-            'meta_keywords' => new sfMainPlugin\Validator\sfValidatorString(array('max_length' => 1000)),
-            'meta_description' => new sfMainPlugin\Validator\sfValidatorString(array('max_length' => 2000)),
+            'meta_title' => new sfMainPlugin\Validator\sfValidatorString(array('required' => false, 'max_length' => 255)),
+            'meta_keywords' => new sfMainPlugin\Validator\sfValidatorString(array('required' => false, 'max_length' => 1000)),
+            'meta_description' => new sfMainPlugin\Validator\sfValidatorString(array('required' => false, 'max_length' => 2000)),
         ));
 
         $this->widgetSchema->setNameFormat('blog_post[%s]');
